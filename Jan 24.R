f1cbeb7856744e383adee6bae84679d51caa544d
WhitneySabb <89420814+WhitneySabb@users.noreply.github.com>
2022-01-24 11:36:06 -0500

    Add files via upload

---
+++ Jan 24.R
@@ -0,0 +1,3 @@
+# Author: Sabb, Date: Jan 24 2022, Purpose: National COVID Dataset Link
+
+data <- read.csv("C:\\Users\\wsabb\\OneDrive\\Documents\\Spring 2022\\Probability and Statistics 2 - Pawar\\national-history.csv")
